1c59294af4c39387bed31ddfbd4d2d0a0eaf9f38
PayodhiAgrawal <payodhiagrawal91@gmail.com>
2020-08-01 19:28:17 +0530

    learning es6+

--- es6+/index.js
+++ es6+/index.js
@@ -6,7 +6,7 @@ let n1=5;
 let n2=4;
 const fullName=`${n1+n2} ${word1} ${word2}`;
 console.log(fullName);
-let example1='Hello \n' + 'World';
+let example1=`${word1}
+${word2}`;
 console.log(example1);
-let x=document.getElementById('example')
-x.innerText = example1;
+document.getElementById('example').innerText = example1;
